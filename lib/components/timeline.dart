7e7d59470593b7f1cad449eab8147878d55913ed
johng149 <jg843@cornell.edu>
2022-12-27 16:48:08 -0500

    Implemented point notifier

---
+++ lib/components/timeline.dart
@@ -0,0 +1,5 @@
+
+///A widget that displays zero or more points of interest on parallel lines
+///
+///The [Timeline] widget determines which points should be considered for 
+///rendering based on given 
