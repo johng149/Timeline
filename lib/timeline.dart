0f5f22f91e50d66267197e2f7511bca5d6e1f8b1
johng149 <jg843@cornell.edu>
2022-12-28 11:12:27 -0500

    Export library

--- lib/timeline.dart
+++ lib/timeline.dart
@@ -1,1 +1,13 @@
 library timeline;
+
+//we need to export the timeline widget, point class, viewrange class, the
+//various notifiers and the typedef for function that creates a point
+//while making sure to only show the necessary names to avoid polluting the
+//namespace
+export 'components/timeline.dart' show Timeline;
+export 'models/point/point.dart' show Point;
+export 'models/viewrange/viewrange.dart' show ViewRange;
+export 'providers/group_notifier.dart' show GroupNotifier;
+export 'providers/point_notifier.dart' show PointNotifier;
+export 'providers/viewrange_notifier.dart' show ViewRangeNotifier;
+export 'definitions/create_point.dart' show CreatePoint;
